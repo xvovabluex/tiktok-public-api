553d7990b5fa37829618f270502b9778eaba5920
xvovabluex <95418222+xvovabluex@users.noreply.github.com>
2025-04-03 23:07:20 +0300

    Update lol.py

--- api/lol.py
+++ api/lol.py
@@ -12,13 +12,13 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
         #self.send_header("Content-type", "text/html")
         #self.end_headers()
         
-        self.wfile.write(bytes(requests.post(url).text, "utf-8"))
+        self.wfile.write(bytes(requests.get(url).text, "utf-8"))
 
     def do_POST(self):
         parsed_url = urllib.parse.urlparse(self.path)
         query_params = urllib.parse.parse_qs(parsed_url.query)
         url = query_params.get("video", [None])[0]
 
-        self.wfile.write(bytes(requests.post(url).text, "utf-8"))
+        self.wfile.write(bytes(requests.get(url).text, "utf-8"))
 
 handler = app = RequestHandler
